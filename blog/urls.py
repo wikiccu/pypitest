e8fbfd17cacc04b70f4b1d67aef01a1179988452
wikiccu <privatemx.2010@gmail.com>
2020-03-09 20:15:29 +0330

    initilize

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from users import views as user_views
+
+urlpatterns = [
+    path('',views.index, name='blog-index'),
+    path('register/', user_views.register, name="register"),
+    path('about/',views.about, name='blog-about'),
+
+]
---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render 
+from .models import Post 
+
+def  index (request):
+   context = {
+      'posts' : Post.objects.all(),
+   }
+   return render(request,'blog/index.html', context)
+
+def  about (request):
+   return render(request, 'blog/about.html', {'title' :'about'})
+
